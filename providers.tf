58a806be71bfb6a99272c67730eddd96f5a66c81
nikhilmudda <nikhilmudda20@gmail.com>
2026-09-11 08:17:33 +0530

    Configure AWS provider in Terraform

--- providers.tf
+++ providers.tf
@@ -1,1 +1,12 @@
-- Tell Terraform we're working with AWS
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>=1.5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
