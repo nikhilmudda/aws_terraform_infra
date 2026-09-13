15fd62de3b0161f0a6bcb0b285896993fdfa495e
nikhilmudda <nikhilmudda20@gmail.com>
2026-09-13 07:40:57 +0530

    Create IAM user and S3 access policy
    Added IAM user, S3 access policy, and policy attachment.

--- iam.tf
+++ iam.tf
@@ -1,20 +1,42 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
+# Create the IAM User
 resource "aws_iam_user" "my_user" {
   name = "terraform-user"
 
   tags = {
     Name = "Terraform IAM User"
   }
+}
+
+# Create the S3 policy
+resource "aws_iam_policy" "s3_access" {
+  name        = "S3AccessPolicy"
+  description = "Allow access to my existing S3 bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = "arn:aws:s3:::my-existing-bucket"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "arn:aws:s3:::my-existing-bucket/*"
+      }
+    ]
+  })
+}
+
+# Attach policy to the IAM user created above
+resource "aws_iam_user_policy_attachment" "s3_access" {
+  user       = aws_iam_user.my_user.name
+  policy_arn = aws_iam_policy.s3_access.arn
 }
