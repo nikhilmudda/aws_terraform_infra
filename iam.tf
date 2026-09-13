ff718e837ddbca86e52c3b054f5e77542eee9d08
nikhilmudda <nikhilmudda20@gmail.com>
2026-09-13 07:26:10 +0530

    Initialize IAM user with Terraform
    Add Terraform configuration for AWS IAM user.

--- iam.tf
+++ iam.tf
@@ -1,1 +1,20 @@
-IAM roles/policies
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_user" "my_user" {
+  name = "terraform-user"
+
+  tags = {
+    Name = "Terraform IAM User"
+  }
+}
